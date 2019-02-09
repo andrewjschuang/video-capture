e465852848d7436cac1b7bb24dc03cf10b0e1aa0
Andrew Chuang <andrewjschuang@gmail.com>
2019-02-09 21:57:51 -0200

    fix file not image error

--- video_capture.py
+++ video_capture.py
@@ -4,15 +4,18 @@ import requests
 import time
 import os
 from PIL import Image
+from io import BytesIO
 
 video_source = os.getenv('VIDEO_SOURCE', 0)
 url = os.getenv('BMSISP', 'http://localhost:5000')
 
-def send(image):
+def send(filename):
     print('sending frame')
     try:
-        r = requests.post(url, files={'file': image})
-        del image
+        img = open(filename, 'rb')
+        r = requests.post(url, files={'file': img})
+        img.close()
+        threading.Thread(target=os.remove, args=(filename,)).start()
         print('frame sent. status code: %s' % r.status_code)
     except Exception as e:
         print(e)
@@ -28,10 +31,14 @@ if __name__ == '__main__':
         print('error opening video source %s' % video_source)
         exit(1)
 
+    n = 0
     while True:
         ret, frame = video_capture.read()
         if ret:
             print('got new frame')
             pil_image = Image.fromarray(frame)
-            threading.Thread(target=send, args=(pil_image.tobytes(),)).start()
+            filename = 'frames/img' + str(n) + '.jpg'
+            pil_image.save(filename)
+            threading.Thread(target=send, args=(filename,)).start()
+            n += 1
         time.sleep(0.5)
