f4c709640872f80b7b8ec2223746a1106f17d4a5
Andrew Chuang <andrewjschuang@gmail.com>
2024-02-17 11:58:25 -0300

    refactoring

--- server.py
+++ server.py
@@ -2,51 +2,63 @@ from flask import Flask, request
 from video_capture import VideoCapture
 import threading
 import os
+import logging
 
 app = Flask(__name__)
 vc = VideoCapture()
+logging.basicConfig(level=logging.INFO)
 
-def remove(f):
+
+def remove(file):
     try:
-        os.remove(f)
-    except:
-        pass
+        os.remove(file)
+    except OSError as e:
+        logging.error(f"Error removing file {file}: {e}")
+
 
 @app.route('/', methods=['GET'])
 def index():
     return 'Use /start or /stop!'
 
+
 @app.route('/configure', methods=['GET'])
 def configure():
-    if request.args.get('url'):
-        vc.url = request.args.get('url')
-    if request.args.get('video_source'):
+    url = request.args.get('url')
+    video_source = request.args.get('video_source', '0')
+
+    if url:
+        vc.url = url
+
+    if video_source:
         try:
-            vc.configure(int(request.args.get('video_source')))
-        except:
-            vc.configure(request.args.get('video_source'))
+            vc.configure(int(video_source))
+        except ValueError:
+            vc.configure(video_source)
 
     return 'configured'
+
 
 @app.route('/start', methods=['GET'])
 def start():
     vc.run = True
-    threading.Thread(target=vc.start).start()
-    print('started')
-
+    threading.Thread(target=vc.start, daemon=True).start()
+    logging.info('Started')
     return 'started'
+
 
 @app.route('/stop', methods=['GET'])
 def stop():
     vc.run = False
-    clean = False
-    if clean:
-        print('cleaning up...')
-        while len(vc.files) > 0:
-            f = vc.files.pop()
-            threading.Thread(target=remove, args=(f,)).start()
-    print('stopped')
+    if request.args.get('clean', 'false').lower() == 'true':
+        logging.info('Cleaning up...')
+        while vc.files:
+            file = vc.files.pop()
+            threading.Thread(target=remove, args=(file,), daemon=True).start()
+    logging.info('Stopped')
     return 'stopped'
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    host = os.getenv('FLASK_HOST', '0.0.0.0')
+    port = os.getenv('FLASK_PORT', 5001)
+    app.run(host=host, port=port, debug=bool(os.getenv('FLASK_DEBUG', False)))
--- video_capture.py
+++ video_capture.py
@@ -3,47 +3,67 @@ import threading
 import requests
 import time
 import os
-import random
+import logging
 from PIL import Image
+from queue import Queue
+
+logging.basicConfig(level=logging.INFO)
 
 class VideoCapture():
     def __init__(self, url='http://localhost:5000/api', video_source=0):
-        try:
-            os.mkdir('frames')
-        except:
-            pass
+        self.ensure_directory_exists('frames')
         self.files = ['frames/' + x for x in os.listdir('frames')]
         self.run = True
         self.url = url
-        self.video_capture = cv2.VideoCapture(video_source)
+        self.video_capture = self.initialize_video_capture(video_source)
+        self.frame_queue = Queue()
+        self.sender_thread = threading.Thread(target=self.send_frames, daemon=True)
+        self.sender_thread.start()
+
+
+    def ensure_directory_exists(self, directory):
+        if not os.path.exists(directory):
+            try:
+                os.mkdir(directory)
+            except FileExistsError:
+                pass  # Directory already exists
+
+
+    def initialize_video_capture(self, video_source):
+        video_capture = cv2.VideoCapture(video_source)
+        if not video_capture.isOpened():
+            logging.error(f"Error opening video source {video_source}")
+            raise ValueError(f"Error opening video source {video_source}")
+        return video_capture
+
 
     def configure(self, video_source):
         self.video_capture.release()
-        try:
-            self.video_capture = cv2.VideoCapture(video_source)
-            if not self.video_capture.isOpened():
-                print('error opening video source %s' % video_source)
-                exit(1)
-        except:
-            print('couldnt configure video_source')
+        self.video_capture = self.initialize_video_capture(video_source)
+
 
     def start(self, sleep=0.5):
         while self.run:
             ret, frame = self.video_capture.read()
             if ret:
-                print('got new frame')
-                pil_image = Image.fromarray(frame)
-                filename = f'frames/{time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())}.jpg'
-                pil_image.save(filename)
-                self.files.append(filename)
-                threading.Thread(target=self.send, args=(filename,)).start()
+                logging.info('Got new frame')
+                current_datetime = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+                file = f'frames/{current_datetime}.jpg'
+                Image.fromarray(frame).save(file)
+                self.files.append(file)
+                self.frame_queue.put(file)
             time.sleep(sleep)
 
-    def send(self, f):
-        print('sending frame')
-        try:
-            with open(f, 'rb') as img:
-                r = requests.post(self.url, files={'file': img})
-                print(r.text)
-        except Exception as e:
-            print(e)
+
+    def send_frames(self):
+        while self.run or not self.frame_queue.empty():
+            file = self.frame_queue.get()
+            try:
+                with open(file, 'rb') as img:
+                    r = requests.post(self.url, files={'file': img})
+                    if r.ok:
+                        logging.info(f"Sent frame {file}")
+                    else:
+                        logging.error(f"Error sending frame {file}: {r.status_code} {r.text}")
+            except Exception as e:
+                logging.error(f"Error sending frame {file}: {e}")
