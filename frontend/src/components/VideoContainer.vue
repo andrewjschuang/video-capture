2c73b8817d278a7c0690558c042d2d27888c4493
Andrew Chuang <andrewjschuang@gmail.com>
2024-02-21 08:07:33 -0300

    show images

--- frontend/src/components/VideoContainer.vue
+++ frontend/src/components/VideoContainer.vue
@@ -8,10 +8,29 @@
             <button class="button" @click="stopRecording">Stop</button>
         </div>
     </div>
+    <div class="image-stack">
+        <div v-for="(image, index) in images" :key="image.id" class="image-container">
+            <img v-if="index < MAX_IMAGES" :src="image.src" :alt="`Image ${index + 1}`" class="image" />
+        </div>
+    </div>
 </template>
 
 <script setup>
 import { ref, computed } from 'vue';
+
+import image from '@/assets/images/2024-02-17_12-01-18.jpg';
+const imageSrc = ref('');
+fetch(image)
+    .then((res) => res.blob())
+    .then((blob) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = function () {
+            const base64data = reader.result;
+            imageSrc.value = base64data;
+        }
+    });
+
 
 const recording = ref(false);
 const border = computed(() => recording.value ? '2px solid red' : '2px solid #f0f0f0');
@@ -27,12 +46,18 @@ const stopRecording = async () => {
     recording.value = false;
     console.log('Stop recording...');
 };
+
+const MAX_IMAGES = 20;
+const images = ref([]);
+for (let i = 1; i <= 100; i++) {
+    images.value.push({ id: i, src: imageSrc });
+}
 </script>
 
 <style scoped>
 .default-box {
     width: 80%;
-    height: 400px;
+    height: 500px;
     margin: auto;
     display: flex;
     justify-content: center;
@@ -58,5 +83,41 @@ const stopRecording = async () => {
 
 .button:hover {
     background-color: #555;
+}
+
+.image-stack {
+    margin-top: 50px;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    max-width: 100vw;
+    white-space: nowrap;
+    position: relative;
+    display: flex;
+    flex-wrap: nowrap;
+    transition: transform 0.3s ease-in-out;
+}
+
+.image {
+    display: block;
+    flex: 0 0 auto;
+    width: 300px;
+    height: auto;
+    object-fit: cover;
+    border-radius: 10px;
+    opacity: 0.6;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    margin-right: 20px;
+}
+
+/* .image:last-child {
+    margin-right: 0px;
+} */
+
+.image:hover {
+    transform: scale(1.05);
+    z-index: 10;
+    position: relative;
+    border-radius: 15px;
+    opacity: 1;
 }
 </style>
